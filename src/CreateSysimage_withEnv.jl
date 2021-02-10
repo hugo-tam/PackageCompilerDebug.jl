9e5ef174ea2738412f15ce950d6354f5d39cb8a9
hugo-sapiat <hugo.tam@sapiat.com>
2021-02-10 17:09:08 +0000

    looks like something works

--- src/CreateSysimage_withEnv.jl
+++ src/CreateSysimage_withEnv.jl
@@ -6,10 +6,7 @@ mkpath(env_path)
 Pkg.activate(env_path)
 Pkg.add(PackageSpec(path="$(dirname(@__DIR__))", rev="master"))
 
-# create a system image, which should fail when executing the precompile stage
+# create a system image
 using PackageCompiler
 mkpath("$(dirname(@__DIR__))/compiled")
 create_sysimage(:PackageCompilerDebug, sysimage_path="$(dirname(@__DIR__))/compiled/JuliaSimulatorDebug.so", precompile_execution_file="$(@__DIR__)/CreateSysimage_PrecompileExe.jl")
-
-# # alternatively, without precompile, an image can be created by it will fail at runtime
-# create_sysimage(:PackageCompilerDebug, sysimage_path="$(dirname(@__DIR__))/compiled/JuliaSimulatorDebug.so")
