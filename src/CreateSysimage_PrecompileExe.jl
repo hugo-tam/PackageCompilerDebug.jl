f3d92d66fb19d89cfdf0706464cc983f8ab29bfc
hugo-sapiat <hugo.tam@sapiat.com>
2021-02-17 15:56:21 +0000

    update precompile.jl

--- src/CreateSysimage_PrecompileExe.jl
+++ src/CreateSysimage_PrecompileExe.jl
@@ -1,2 +1,2 @@
 using PackageCompilerDebug
-@show plot_x_square = plot_dataframe(collect(1:10), collect(1:10) .^ 2)
+@show plot_x_square = plot_dataframe("$(dirname(@__DIR__))/compiled/test.svg", collect(1:10), collect(1:10) .^ 2)
